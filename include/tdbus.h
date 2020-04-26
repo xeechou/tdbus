b0332618efe0ce10d124d4ec148636bd3e660025
Xichen Zhou <xzhou@xeechou.net>
2020-04-26 16:59:30 -0400

    folding declaration

--- include/tdbus.h
+++ include/tdbus.h
@@ -102,18 +102,22 @@ struct tdbus_call_answer {
 	tdbus_read_call_f reader;
 };
 
-struct tdbus *tdbus_new(enum TDBUS_TYPE type);
+struct tdbus *
+tdbus_new(enum TDBUS_TYPE type);
 
-struct tdbus *tdbus_new_server(enum TDBUS_TYPE, const char *bus_name);
+struct tdbus *
+tdbus_new_server(enum TDBUS_TYPE, const char *bus_name);
 
-void tdbus_delete(struct tdbus *bus);
+void
+tdbus_delete(struct tdbus *bus);
 
-bool tdbus_match_signal(struct tdbus *bus,const char *sender,
-                        const char *iface, const char *member,
-                        const char *path, void *user_data,
-                        tdbus_read_signal_f read_signal);
-
-void tdbus_free_message(struct tdbus_message *message);
+bool
+tdbus_match_signal(struct tdbus *bus,const char *sender,
+                   const char *iface, const char *member,
+                   const char *path, void *user_data,
+                   tdbus_read_signal_f read_signal);
+void
+tdbus_free_message(struct tdbus_message *message);
 
 
 /**
@@ -132,11 +136,13 @@ void tdbus_free_message(struct tdbus_message *message);
  * Currently @tdbus_readv and @tdbus_writev only supports basic types.
  *
  */
-void tdbus_readv(const struct tdbus_message *msg, const char *format,
-                 va_list ap);
+void
+tdbus_readv(const struct tdbus_message *msg, const char *format,
+            va_list ap);
 
-static inline void tdbus_read(const struct tdbus_message *msg,
-                              const char *format, ...)
+static inline void
+tdbus_read(const struct tdbus_message *msg,
+           const char *format, ...)
 {
 	va_list ap;
 	va_start(ap, format);
@@ -144,15 +150,18 @@ static inline void tdbus_read(const struct tdbus_message *msg,
 	va_end(ap);
 }
 
-bool tdbus_read_with_iter(const struct tdbus_message *msg, const char *format,
-                          struct tdbus_message_itr *itr);
+bool
+tdbus_read_with_iter(const struct tdbus_message *msg, const char *format,
+                     struct tdbus_message_itr *itr);
 
 /**
  * @brief writing a message
  */
-bool tdbus_writev(struct tdbus_message *message, const char *format, va_list ap);
+bool
+tdbus_writev(struct tdbus_message *message, const char *format, va_list ap);
 
-static inline bool tdbus_write(struct tdbus_message *msg, const char *format, ...)
+static inline bool
+tdbus_write(struct tdbus_message *msg, const char *format, ...)
 {
 	bool ret;
 	va_list ap;
@@ -163,32 +172,37 @@ static inline bool tdbus_write(struct tdbus_message *msg, const char *format, ..
 	return ret;
 }
 
-bool tdbus_write_with_itr(struct tdbus_message *msg, const char *format,
-                          struct tdbus_message_itr *itr);
+bool
+tdbus_write_with_itr(struct tdbus_message *msg, const char *format,
+                     struct tdbus_message_itr *itr);
 
 /**
  * @brief generate a method call message for writing,
 
  * use tdbus_write to produce the content of the message
  */
-struct tdbus_message *tdbus_call_method(const char *dest, const char *path,
-                                        const char *interface, const char *method,
-                                        tdbus_read_reply_f reply, void *user_data);
+struct tdbus_message *
+tdbus_call_method(const char *dest, const char *path,
+                  const char *interface, const char *method,
+                  tdbus_read_reply_f reply, void *user_data);
 
 /**
  * @brief generate a reply message on the server
  */
-struct tdbus_message *tdbus_reply_method(const struct tdbus_message *reply_to,
-                                         const char *err_name);
+struct tdbus_message *
+tdbus_reply_method(const struct tdbus_message *reply_to,
+                   const char *err_name);
 
-void tdbus_send_message(struct tdbus *bus, struct tdbus_message *msg);
+void
+tdbus_send_message(struct tdbus *bus, struct tdbus_message *msg);
 
 /**
  * @brief adding methods and register the object path
  */
-bool tdbus_server_add_methods(struct tdbus *bus, const char *obj_path,
-                              unsigned int n_methods,
-                              struct tdbus_call_answer *answers);
+bool
+tdbus_server_add_methods(struct tdbus *bus, const char *obj_path,
+                         unsigned int n_methods,
+                         struct tdbus_call_answer *answers);
 
 /**
  * dbus watchers
@@ -207,25 +221,26 @@ typedef void (*tdbus_rm_watch_f)(void *user_data, int unix_fd, struct tdbus *bus
  *
  * In the watches, call @tdbus_handle_watch to process the dbus internals.
  */
-void tdbus_set_nonblock(struct tdbus *bus, void *data,
-                        tdbus_add_watch_f addf,
-                        tdbus_ch_watch_f chf,
-                        tdbus_rm_watch_f rmf);
+void
+tdbus_set_nonblock(struct tdbus *bus, void *data,
+                   tdbus_add_watch_f addf,
+                   tdbus_ch_watch_f chf,
+                   tdbus_rm_watch_f rmf);
+void
+tdbus_handle_watch(struct tdbus *bus, void *watch_data);
 
+void
+tdbus_watch_set_user_data(void *watch_data, void *user_data);
 
-void tdbus_handle_watch(struct tdbus *bus, void *watch_data);
-
-void tdbus_watch_set_user_data(void *watch_data, void *user_data);
-
-void *tdbus_watch_get_user_data(void *watch_data);
+void *
+tdbus_watch_get_user_data(void *watch_data);
 
 /**
  * @brief dispatch the mainloop once, this is an idle run. I cant believe they
  * dont have polling
  */
-void tdbus_dispatch_once(struct tdbus *bus);
-
-
+void
+tdbus_dispatch_once(struct tdbus *bus);
 
 
 #ifdef __cplusplus
